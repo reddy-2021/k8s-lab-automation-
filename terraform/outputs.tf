7e1e04f662e26f930fd24f8b93365c205d854704
reddy-2021 <78398287+reddy-2021@users.noreply.github.com>
2026-04-15 07:11:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "master_public_ip" {
+output "master_ip" {
   value = aws_instance.master.public_ip
 }
 
-output "worker_public_ips" {
+output "worker_ips" {
   value = aws_instance.worker[*].public_ip
 }
